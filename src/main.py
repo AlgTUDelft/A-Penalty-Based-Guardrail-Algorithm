f01be7c9ce5673bd7b2cbe0d653dda53464ffa82
Ksenija <K.Stepanovic@tudelft.nl>
2025-05-13 16:41:44 +0200

    Uncommenting

--- src/main.py
+++ src/main.py
@@ -7,12 +7,12 @@ from nonlinear.plot_nonlinear import plot as plot_nonlinear
 from dnn.plot_dnn import plot as plot_dnn
 
 if __name__ == "__main__":
-    # executing algorithms
-    #linear(functions=["fun_1", "fun_2", "fun_3"])
-    #nonlinear(functions=["fun_4", "fun_5", "fun_6"])
-    #dnn()
+    # running algorithms
+    linear(functions=["fun_1", "fun_2", "fun_3"])
+    nonlinear(functions=["fun_4", "fun_5", "fun_6"])
+    dnn()
 
     # plotting results
-    # plot_linear(function="fun_1")
-    # plot_nonlinear(function="fun_8")
+    plot_linear(function="fun_1")
+    plot_nonlinear(function="fun_8")
     plot_dnn()
